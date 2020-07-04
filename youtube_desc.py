089b9accfb9b2764434d87627ac1547abf81786f
Hoel Kervadec <hoel@kervadec.science>
2020-07-04 15:02:46 -0400

    Update youtube description generation

--- youtube_desc.py
+++ youtube_desc.py
@@ -18,10 +18,19 @@ if __name__ == "__main__":
     with open(papers_path, 'r') as pf:
         raw_papers = json.load(pf)
 
-    papers: Dict[int, Paper] = {int(k): Paper(**v) for (k, v) in raw_papers.items()}
+    papers: Dict[int, Paper] = {int(k): Paper(ignore_schedule=True, **v) for (k, v) in raw_papers.items()}
 
     for paper in papers.values():
         first_author: str = paper.authors[0].split(' ')[-1]
+        mode: str
+        if paper.short:
+            mode = "Short presentation"
+        else:
+            if paper.oral:
+                mode = "Oral presentation"
+            else:
+                mode = "Spotlight presentation"
+
         print("\n>>>")
-        print(f"MIDL 2020, {paper.conf_id}, {first_author} et al. Teaser")
-        print(f"{paper.conf_id} - {paper.title}\n\n{', '.join(paper.authors)}")
+        print(f"MIDL 2020, {paper.conf_id}, {first_author} et al. {mode}")
+        print(f"{paper.conf_id} - {paper.title}\n\n{', '.join(paper.authors)}\n\nConference page: https://2020.midl.io/{paper.url}\nPDF: https://openreview.net/pdf?id={paper.or_id}\n\nAbstract: {paper.sanitized_abstract}")
